99d9b09278f3b676eb53fb471395c6a488429c10
Tom <tomc@tomvisions.com>
2024-05-15 11:47:24 -0400

    update the api gateway

--- s3Bucket/lib/api-gateway-stack.ts
+++ s3Bucket/lib/api-gateway-stack.ts
@@ -36,7 +36,7 @@ export class ApiGatewayStack extends cdk.Stack {
     }
 
     private async generateResource(path:string) {
-        this._apiResource = this._restApi.root.addResource(`test`, {
+        this._apiResource = this._restApi.root.addResource(`testing`, {
             defaultIntegration: this._lambdaIntegration
         });
         this.generateMethod();
