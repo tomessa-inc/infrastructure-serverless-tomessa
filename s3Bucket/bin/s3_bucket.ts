1494345b329daf053914cde3f13815d00c290df1
Tom <tomc@tomvisions.com>
2024-05-15 10:32:52 -0400

    updated call

--- s3Bucket/bin/s3_bucket.ts
+++ s3Bucket/bin/s3_bucket.ts
@@ -4,6 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { IAMRoleStack} from "../lib/iam-role-stack";
 import { S3BucketStack2 } from '../lib/s3_bucket-stack';
 import {TestingStack} from "../lib/Testing";
+import {LambdaStack} from "../lib/lambda-stack";
+import {ApiGatewayStack} from "../lib/api-gateway-stack";
 
 const app = new cdk.App();
 new IAMRoleStack(app, 'IAMRoleStack', {
@@ -21,6 +23,40 @@ new IAMRoleStack(app, 'IAMRoleStack', {
 
     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+new LambdaStack(app, 'LambdaStack', {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
+
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
+
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+new ApiGatewayStack(app, 'ApiGatewayStack', {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
+
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
+
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+
 
 new S3BucketStack2(app, 'S3BucketStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
